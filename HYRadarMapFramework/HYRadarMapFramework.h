f20dc64d2bd8a1b421324f8243a855fd2fc1a6b1
HanYong <HanYong@192.168.1.101>
2019-01-20 15:52:22 +0800

    封装framework

---
+++ HYRadarMapFramework/HYRadarMapFramework.h
@@ -0,0 +1,19 @@
+//
+//  HYRadarMapFramework.h
+//  HYRadarMapFramework
+//
+//  Created by HanYong on 2019/1/20.
+//  Copyright © 2019年 HanYong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HYRadarMapFramework.
+FOUNDATION_EXPORT double HYRadarMapFrameworkVersionNumber;
+
+//! Project version string for HYRadarMapFramework.
+FOUNDATION_EXPORT const unsigned char HYRadarMapFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HYRadarMapFramework/PublicHeader.h>
+
+#import "RadarMapView.h"
